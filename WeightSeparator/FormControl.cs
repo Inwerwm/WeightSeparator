cf2fb5c5e06255eb1b5c7e264c84fe88b9de625a
Inwerwm <saclainvervm@gmail.com>
2022-01-10 12:22:10 +0900

    プロジェクト ファイルを追加します。

---
+++ WeightSeparator/FormControl.cs
@@ -0,0 +1,37 @@
+﻿using PEPExtensions;
+using PEPlugin;
+using PEPlugin.Pmx;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeightSeparator;
+public partial class FormControl : Form
+{
+    IPERunArgs Args { get; }
+
+    public FormControl(IPERunArgs args)
+    {
+        Args = args;
+
+        InitializeComponent();
+        Reload();
+    }
+
+    internal void Reload()
+    {
+        // TODO: プラグインの再読み込み処理を記述します。
+    }
+
+    private void FormControl_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        e.Cancel = true;
+        Hide();
+    }
+}
